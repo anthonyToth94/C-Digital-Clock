ed6603088951947c10a287a051678d69617bbe5a
Antal Toth <anthony.toth94@gmail.com>
2021-04-01 13:08:03 +0200

    Digital Clock

---
+++ DigitalClock/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace DigitalClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 80.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.GreenYellow;
+            this.label1.Location = new System.Drawing.Point(23, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(327, 122);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "12:00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(330, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 63);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "30";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label3.Location = new System.Drawing.Point(49, 283);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(310, 42);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "29 Marcius 2020";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 51.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label4.Location = new System.Drawing.Point(33, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(317, 79);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Saturday";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.ForeColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(23, 173);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(333, 1);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.ForeColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(23, 268);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(333, 1);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.ForeColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(23, 335);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(333, 1);
+            this.panel3.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label5.Location = new System.Drawing.Point(303, 353);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Random";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(431, 387);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "DigitalClock";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ DigitalClock/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DigitalClock
+{
+    public partial class Form1 : Form
+    {
+        int count = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) //Every 1000 => 1ms we need to update the time!
+        {
+            label1.Text = DateTime.Now.ToString("hh:mm");
+            label2.Text = DateTime.Now.ToString("ss");
+            label3.Text = DateTime.Now.ToString("MMM dd yyyy");
+            label4.Text = DateTime.Now.ToString("dddd");                                    //       .    <= X                     
+            label2.Location = new Point(label1.Location.X + label1.Width - 20, label2.Location.Y); //|___|    |__._|  <= Width - 20   
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            Color[] green = new Color[] { Color.GreenYellow, Color.Lime, Color.FromArgb(128, 255, 128), Color.FromArgb(192, 255, 192) };
+            Color[] pink = new Color[] { Color.Magenta,  Color.Fuchsia, Color.FromArgb(255, 128, 128), Color.FromArgb(255, 192, 192) };
+            Color[] blue = new Color[] { Color.Blue, Color.MediumBlue, Color.FromArgb(128, 128, 255), Color.FromArgb(192, 192, 255) };
+            Color[] yellow = new Color[] { Color.Gold, Color.Yellow, Color.FromArgb(255, 255, 128), Color.FromArgb(255, 255, 192) };
+        
+                if(count == 1)
+                {
+                    label1.ForeColor = pink[0];
+                    label2.ForeColor = pink[1];
+                    label3.ForeColor = pink[2];
+                    label4.ForeColor = pink[3];         
+                }
+                else if(count == 2)
+                {
+                    label1.ForeColor = blue[0];
+                    label2.ForeColor = blue[1];
+                    label3.ForeColor = blue[2];
+                    label4.ForeColor = blue[3];
+                }
+                else if(count == 3)
+                {
+                    label1.ForeColor = yellow[0];  //nagyon zold
+                    label2.ForeColor = yellow[1];   //kiváló zöld
+                    label3.ForeColor = yellow[2];   //halvanyabb
+                    label4.ForeColor = yellow[3]; //leghalvanyabb
+                }
+                else
+                {
+                    count = 0;
+                    if (count == 0)
+                    {
+                        label1.ForeColor = green[0];  //nagyon zold
+                        label2.ForeColor = green[1];   //kiváló zöld
+                        label3.ForeColor = green[2];   //halvanyabb
+                        label4.ForeColor = green[3]; //leghalvanyabb
+                    }
+                }
+                count++;
+
+            //switch (r.Next(1,5))
+            //{
+            //    case 1:
+            //        for (int i = 0; i < pink.Length; i++)
+            //        {
+            //            label1.ForeColor = pink[0];
+            //            label2.ForeColor = pink[1];
+            //            label3.ForeColor = pink[2];
+            //            label4.ForeColor = pink[3];
+            //        }
+            //        break;
+
+            //    case 2:
+            //        for (int i = 0; i < blue.Length; i++)
+            //        {
+            //            label1.ForeColor = blue[0];
+            //            label2.ForeColor = blue[1];
+            //            label3.ForeColor = blue[2];
+            //            label4.ForeColor = blue[3];
+            //        }
+            //        break;
+
+            //    case 3:
+            //        for (int i = 0; i < green.Length; i++)
+            //        {
+            //            label1.ForeColor = green[0];  //nagyon zold
+            //            label2.ForeColor = green[1];   //kiváló zöld
+            //            label3.ForeColor = green[2];   //halvanyabb
+            //            label4.ForeColor = green[3]; //leghalvanyabb
+            //        }
+            //        break;
+
+            //    case 4:
+            //        for (int i = 0; i < yellow.Length; i++)
+            //        {
+            //            label1.ForeColor = yellow[0];  //nagyon zold
+            //            label2.ForeColor = yellow[1];   //kiváló zöld
+            //            label3.ForeColor = yellow[2];   //halvanyabb
+            //            label4.ForeColor = yellow[3]; //leghalvanyabb
+            //        }
+            //        break;
+            //}
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Escape)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
